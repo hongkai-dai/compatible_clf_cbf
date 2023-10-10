eaa8630604e3e31231e79611a403f29103b96e0f
Hongkai Dai <daihongkai@gmail.com>
2023-10-09 23:08:46 -0700

    Fix a bug in CompatibleLagrangian::get_result().

--- compatible_clf_cbf/utils.py
+++ compatible_clf_cbf/utils.py
@@ -1,6 +1,8 @@
+from typing import Optional, Union
 import numpy as np
 
 import pydrake.symbolic as sym
+import pydrake.solvers as solvers
 
 
 def check_array_of_polynomials(p: np.ndarray, x_set: sym.Variables) -> None:
@@ -21,3 +23,34 @@ def check_polynomial_arrays_equal(p: np.ndarray, q: np.ndarray, tol: float):
     assert p.shape == q.shape
     for p_i, q_i in zip(p.flat, q.flat):
         assert p_i.CoefficientsAlmostEqual(q_i, tol)
+
+
+def get_polynomial_result(
+    result: solvers.MathematicalProgramResult,
+    p: Union[sym.Polynomial, np.ndarray],
+    coefficient_tol: Optional[float] = None,
+) -> Union[sym.Polynomial, np.ndarray]:
+    """
+    Given a MathematicalProgramResult and an array of symbolic Polynomials
+    (or a single symbolic Polynomial), return the result of these symbolic
+    Polynomials. Remove the terms in the polynomials if the absolute vlues of
+    the coefficients are <= coefficient_tol.
+    """
+    if isinstance(p, sym.Polynomial):
+        p_result = result.GetSolution(p)
+        if coefficient_tol is not None:
+            return p_result.RemoveTermsWithSmallCoefficients(coefficient_tol)
+        else:
+            return p_result
+    else:
+        p_result = np.array([result.GetSolution(p_i) for p_i in p.flat]).reshape(
+            p.shape
+        )
+        if coefficient_tol is not None:
+            p_result = np.array(
+                [
+                    p_result_i.RemoveTermsWithSmallCoefficients(coefficient_tol)
+                    for p_result_i in p_result.flat
+                ]
+            ).reshape(p.shape)
+        return p_result
