a1cb2c78c3dab90def0f18ccffdfe9b19636ef0f
Hongkai Dai <daihongkai@gmail.com>
2024-01-21 11:04:56 -0800

    Add the trigonometric dynamics to the nonlinear toy system. (#39)

--- examples/nonlinear_toy/README.md
+++ examples/nonlinear_toy/README.md
@@ -9,3 +9,29 @@ $$
 We will consider the case with or without the input limits on $u$.
 
 This system is introduced in *Searching for control Lyapunov functions using sums-of-squares programming* by Weehong Tan and Andrew Packard.
+
+I _think_ this system is the taylor expansion of the following system
+$$
+\begin{align*}
+\dot{x}_0 = &u\\
+\dot{x}_1=&-\sin x_0 -u
+\end{align*}
+$$
+
+So another way to model this system is to use the trigonometric state
+$$
+\begin{align*}
+\bar{x}_0 =& \sin x_0\\
+\bar{x}_1 =& \cos x_0 -1\\
+\bar{x}_2 =& x_1
+\end{align*}
+$$ 
+Note that the new state $\bar{x}$ has the equilibrium $\bar{x}^*=0$.
+The dynamics is
+$$
+\begin{align*}
+\dot{\bar{x}}_0 =& (\bar{x}_1+1)u\\
+\dot{\bar{x}}_1=&-\bar{x}_0u\\
+\dot{\bar{x}}_2=&-\bar{x}_0 -u
+\end{align*}
+$$
--- examples/nonlinear_toy/toy_system.py
+++ examples/nonlinear_toy/toy_system.py
@@ -32,3 +32,27 @@ def affine_dynamics(x: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         g = np.array([[1], [-1]])
 
     return (f, g)
+
+
+def affine_trig_poly_state_constraints(x: np.ndarray) -> sym.Polynomial:
+    """
+    With the state x̅ = [sinx₀, cosx₀−1, x₁], the state constraint is x̅₀² + (x̅₁+1)²=1
+    """
+    return sym.Polynomial(x[0] ** 2 + x[1] ** 2 + 2 * x[1])
+
+
+def affine_trig_poly_dynamics(x: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    With the trigonometric state x̅ = [sinx₀, cosx₀−1, x₁], we can write the
+    dynamics as affine form f(x̅) + g(x̅)u
+    """
+    assert x.shape == (3,)
+    if x.dtype == object:
+        f = np.array([sym.Polynomial(), sym.Polynomial(), sym.Polynomial(-x[0])])
+        g = np.array(
+            [[sym.Polynomial(x[1] + 1)], [sym.Polynomial(-x[0])], [sym.Polynomial(-1)]]
+        )
+    else:
+        f = np.array([0, 0, -x[0]])
+        g = np.array([[x[1] + 1], [-x[0]], [-1]])
+    return f, g
