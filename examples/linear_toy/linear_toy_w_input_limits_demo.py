38174b8d24e66642b61df0ba677d279e62ae20ba
Hongkai Dai <daihongkai@gmail.com>
2024-03-01 21:19:47 +0800

    Add linear_toy_w_input_limits_demo.

--- examples/linear_toy/linear_toy_w_input_limits_demo.py
+++ examples/linear_toy/linear_toy_w_input_limits_demo.py
@@ -1,5 +1,6 @@
 """
-We search for compatible CLF and CBF for a 2D linear system.
+We search for compatible CLF and CBF for a 2D linear system. The system has input
+limits.
 """
 
 from typing import List, Tuple
@@ -7,7 +8,7 @@ from typing import List, Tuple
 import numpy as np
 
 import pydrake.systems.controllers
-import pydrake.solvers
+import pydrake.solvers as solvers
 import pydrake.symbolic as sym
 
 from compatible_clf_cbf import clf_cbf
@@ -19,10 +20,10 @@ def search_compatible_lagrangians(
     b: np.ndarray,
     kappa_V: float,
     kappa_b: np.ndarray,
+    barrier_eps: np.ndarray,
 ) -> clf_cbf.CompatibleLagrangians:
     y_size = dut.y.size
 
-    # Search for the
     lagrangian_degrees = clf_cbf.CompatibleLagrangianDegrees(
         lambda_y=[
             clf_cbf.CompatibleLagrangianDegrees.Degree(x=2, y=0) for _ in range(dut.nu)
@@ -36,15 +37,15 @@ def search_compatible_lagrangians(
                 for _ in range(y_size)
             ]
         ),
-        rho_minus_V=None,
-        b_plus_eps=None,
+        rho_minus_V=clf_cbf.CompatibleLagrangianDegrees.Degree(x=2, y=2),
+        b_plus_eps=[clf_cbf.CompatibleLagrangianDegrees.Degree(x=2, y=2)],
         state_eq_constraints=None,
     )
     prog, lagrangians = dut.construct_search_compatible_lagrangians(
-        V, b, kappa_V, kappa_b, lagrangian_degrees, barrier_eps=None, local_clf=False
+        V, b, kappa_V, kappa_b, lagrangian_degrees, barrier_eps, local_clf=True
     )
 
-    result = pydrake.solvers.Solve(prog)
+    result = solvers.Solve(prog)
     assert result.is_success()
     lagrangians_result = lagrangians.get_result(result, coefficient_tol=1e-8)
     return lagrangians_result
@@ -67,17 +68,20 @@ def search_lagrangians(
     b: np.ndarray,
     kappa_V: float,
     kappa_b: np.ndarray,
+    barrier_eps: np.ndarray,
 ) -> Tuple[clf_cbf.CompatibleLagrangians, List[clf_cbf.UnsafeRegionLagrangians]]:
-    compatible_lagrangians = search_compatible_lagrangians(dut, V, b, kappa_V, kappa_b)
+    compatible_lagrangians = search_compatible_lagrangians(
+        dut, V, b, kappa_V, kappa_b, barrier_eps
+    )
     barrier_safe_lagrangians = search_barrier_safe_lagrangians(dut, b)
     return (compatible_lagrangians, barrier_safe_lagrangians)
 
 
-def search(use_y_squared: bool):
+def search():
     A = np.array([[1, 2], [-2, 3.0]])
     B = np.array([[1, 0], [0, 1.0]])
 
-    # First compute the LQR controller through Riccati equation.
+    # First compute the LQR controller through Ricatti equation.
     Q = np.eye(2)
     R = np.eye(2)
     K_lqr, S_lqr = pydrake.systems.controllers.LinearQuadraticRegulator(A, B, Q, R)
@@ -95,28 +99,31 @@ def search(use_y_squared: bool):
     alpha = 0.5
     unsafe_regions = [np.array([1.1 * alpha - sym.Polynomial(x.dot(S_lqr @ x))])]
 
+    Au = np.array([[1, 0], [0, 1], [-1, 0], [0, -1.0]])
+    bu = np.array([10, 10, 10, 10])
+
     dut = clf_cbf.CompatibleClfCbf(
         f=f,
         g=g,
         x=x,
         unsafe_regions=unsafe_regions,
-        Au=None,
-        bu=None,
+        Au=Au,
+        bu=bu,
         with_clf=True,
-        use_y_squared=use_y_squared,
+        use_y_squared=True,
     )
 
     V = sym.Polynomial(x.dot(S_lqr @ x))
     b = np.array([alpha - V])
     kappa_V = 0.001
     kappa_b = np.array([0.001])
+    barrier_eps = np.array([1e-4])
 
-    search_lagrangians(dut, V, b, kappa_V, kappa_b)
+    search_lagrangians(dut, V, b, kappa_V, kappa_b, barrier_eps)
 
 
 def main():
-    search(use_y_squared=True)
-    search(use_y_squared=False)
+    search()
 
 
 if __name__ == "__main__":
