5fe9ce863e20c147a3164adb6f80e8bc84c15387
Hongkai Dai <daihongkai@gmail.com>
2025-02-08 20:36:14 -0800

    Add pre-commit-config.yaml

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        subprocess.call(["pre-commit", "install"])
+
 
 # Read the contents of requirements file
 with open("requirements.txt") as f:
@@ -9,4 +18,7 @@ setup(
     version="1.0",
     packages=find_packages(),
     install_requires=requirements,
+    cmdclass={
+        "install": PostInstallCommand,
+    },
 )
